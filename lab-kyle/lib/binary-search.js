ebf9921bee6943067d1759776a84c2cddf41b65c
Kyle Winckler <kyle.winckler@gmail.com>
2017-02-26 12:33:09 -0800

    added tests for binary search

--- lab-kyle/lib/binary-search.js
+++ lab-kyle/lib/binary-search.js
@@ -1,9 +1,27 @@
-module.exports = function(value, array) {
+module.exports = function bs(value, array) {
 
-  if (!array) return false
+  if (!array.length) return false
 
-  let index = math.floor(arr.length / 2)
-  let mid = array[index]
+  let mid = Math.floor(array.length / 2)
+  let curr = array[mid]
 
+  if (value === curr) return true
 
+  if (value < curr) {
+    let newArray = array.slice(0, mid)
+    return bs(value, newArray)
+  } else {
+    let newArray = array.slice(mid + 1, array.length)
+    return bs(value, newArray)
+  }
 }
+
+// let array = [1,2,3,4,5]
+// let test1 = bs(0, array)
+// let test2 = bs(2, array)
+// let test3 = bs(5, array)
+// let test4 = bs(6, array)
+// console.log(test1)
+// console.log(test2)
+// console.log(test3)
+// console.log(test4)
